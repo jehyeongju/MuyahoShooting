ab96dd92772c9bc8b46c884615696b2441fb25a8
PC-18\user <bjnotch@naver.com>
2021-03-30 11:50:56 +0900

    3월 30일

--- Assets/Scripts/Playermove.cs
+++ Assets/Scripts/Playermove.cs
@@ -5,22 +5,41 @@ using UnityEngine;
 public class Playermove : MonoBehaviour
 {
     [SerializeField]
+    private float fireRate = 0.2f;
+    [SerializeField]
     private float speed = 5f;
+    [SerializeField]
+    private Transform bulletPosition = null;
+    [SerializeField]
+    private GameObject bulletPrefab = null;
+    
 
     private Rigidbody2D rigid = null;
     private Vector2 targetPosition = Vector2.zero;
     void Start()
     {
         rigid = GetComponent<Rigidbody2D>();
+        StartCoroutine(Fire());
     }
 
 
     void Update()
     {
-        if (Input.GetMouseButton(0)) { 
+        if (Input.GetMouseButton(0))
+        {
             targetPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
             //igid.MovePosition(targetPosition);
             transform.localPosition = Vector2.MoveTowards(transform.localPosition, targetPosition, speed * Time.deltaTime);
+        }
+    }
+    private IEnumerator Fire()
+    {
+        while (true)
+        {
+            GameObject newBullet = Instantiate(bulletPrefab, bulletPosition);
+            newBullet.transform.position = bulletPosition.position;
+            newBullet.transform.SetParent(null);
+            yield return new WaitForSeconds(fireRate);
         }
     }
 }
